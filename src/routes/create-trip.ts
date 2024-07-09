b7f6cfce5006abf8034fccca898fb79b32ece90e
felprangel <feliperangel897@gmail.com>
2024-07-09 20:49:15 -0300

    envia email depois de criar a viagem

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -3,6 +3,8 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
 import { prisma } from '../lib/prisma'
 import dayjs from 'dayjs'
+import { getMailClient } from '../lib/mail'
+import nodemailer from 'nodemailer'
 
 export async function createTrip(app: FastifyInstance) {
 	app.withTypeProvider<ZodTypeProvider>().post(
@@ -19,7 +21,8 @@ export async function createTrip(app: FastifyInstance) {
 			},
 		},
 		async (request) => {
-			const { destination, starts_at, ends_at, owner_name, owner_email } = request.body
+			const { destination, starts_at, ends_at, owner_name, owner_email } =
+				request.body
 
 			if (dayjs(starts_at).isBefore(new Date())) {
 				throw new Error('Invalid trip start date')
@@ -36,6 +39,24 @@ export async function createTrip(app: FastifyInstance) {
 					ends_at,
 				},
 			})
+
+			const mail = await getMailClient()
+
+			const message = await mail.sendMail({
+				from: {
+					name: 'Equipe plann.er',
+					address: 'suporte@plann.er',
+				},
+				to: {
+					name: owner_name,
+					address: owner_email,
+				},
+				subject: 'Viagem criada com sucesso!',
+				html: `<p>Teste do email</p>`,
+			})
+
+			console.log(nodemailer.getTestMessageUrl(message))
+
 			return { TripId: trip.id }
 		}
 	)
