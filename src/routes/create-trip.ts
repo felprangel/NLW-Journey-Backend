4c53008a0f77f2799b6432bb610c5ced2be005ae
felprangel <feliperangel897@gmail.com>
2024-07-08 22:01:18 -0300

    adiciona schema de validação

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,7 +1,21 @@
 import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
 
 export function createTrip(app: FastifyInstance) {
-	app.post('/trips', async () => {
-        return 'hello world'
-    })
+	app.withTypeProvider<ZodTypeProvider>().post(
+		'/trips',
+		{
+			schema: {
+				body: z.object({
+					destination: z.string().min(4),
+					starts_at: z.coerce.date(),
+					ends_at: z.coerce.date(),
+				}),
+			},
+		},
+		async () => {
+			return 'hello world'
+		}
+	)
 }
