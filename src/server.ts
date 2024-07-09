e30bde1a61ab6269880c4add73d5748e2a6a351a
felprangel <feliperangel897@gmail.com>
2024-07-08 21:47:35 -0300

    adiciona rota de cadastro e de listagem

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
 import fastify from 'fastify'
+import { prisma } from './lib/prisma'
 
 const app = fastify()
 
-app.get('/teste', () => {
-    return 'hello world'
+app.post('/cadastrar', async () => {
+	await prisma.trip.create({
+		data: {
+			destination: 'São Paulo',
+			starts_at: new Date(),
+			ends_at: new Date(),
+		},
+	})
+
+	return 'Registro cadastrado com sucesso!'
+})
+
+app.get('/listar', () => {
+	const trips = prisma.trip.findMany()
+    return trips
 })
 
 app.listen({ port: 3333 }).then(() => {
