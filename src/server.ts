67a450aa38867f431df2866d50235f32f749c764
felprangel <feliperangel897@gmail.com>
2024-07-08 21:53:40 -0300

    cria rota de criação de viagem

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import fastify from 'fastify'
+import { createTrip } from './routes/create-trip'
 
 const app = fastify()
+
+app.register(createTrip)
 
 app.listen({ port: 3333 }).then(() => {
 	console.log('Server is running')
