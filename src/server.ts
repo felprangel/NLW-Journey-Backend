04ca8ee764e040b7c20dc34b0e841b7e7be3bfa0
felprangel <feliperangel897@gmail.com>
2024-07-09 21:56:41 -0300

    adiciona cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { createTrip } from './routes/create-trip'
 import { confirmTrip } from './routes/confirm-trip'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 
 const app = fastify()
+
+app.register(cors, {
+	origin: 'http://localhost:3333', // mudar pro env
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
