cd74f7f9709e23975dc5abcd85798475a49bb3d4
felprangel <feliperangel897@gmail.com>
2024-07-10 21:23:42 -0300

    adiciona rota no server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { createTrip } from './routes/create-trip'
 import { confirmTrip } from './routes/confirm-trip'
+import { confirmParticipant } from './routes/confirm-participant' // Import the confirmParticipant function
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 
 const app = fastify()
@@ -15,6 +16,7 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipant)
 
 app.listen({ port: 3333 }).then(() => {
 	console.log('Server is running')
